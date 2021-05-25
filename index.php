06832767cc02c41c674e09c6de4e54ddea850fad
AyBiCi <rymcymcym100@gmail.com>
2021-05-25 11:15:04 +0200

    Making site "nicer"

--- index.php
+++ index.php
@@ -3,18 +3,48 @@
     <HEAD>
         <TITLE> Minesweeper </TITLE>
         <STYLE>
+            body{
+                padding: 0px;
+                margin: 0px;
+            }
+            #up{
+                width: 100%;
+                height: 10%;
+                background: gray;
+                color: white;
+                font-size: 40px;
+                line-height: 100px;
+                text-align: center;
+                float: left;
+            }
+            #options{
+                width: 300px;
+                height: 700px;
+                float: left;
+                background: darkgray;
+            }
             .tile{
                 width: 32px;
                 height: 32px;
                 float: left;
             }
             #board{
-                margin-left: auto;
-                margin-right: auto;
+                float: left;
             }
         </STYLE>
     </HEAD>
     <BODY>
+        <div id="up">
+           Minesweeper
+        </div>
+        <div id="options">
+            <form action="#" method="get">
+                Mines number: 
+                <input type="number" name="numofmines">
+                <input type="submit" value="Generate new board">
+            </form>
+        </div>
+
         <?php
             require_once("Game/BoardSet.php");
             require_once("Game/BoardRenderer.php");
@@ -23,8 +53,12 @@
             use Minesweeper\Game\BoardRenderer;
             use Minesweeper\Game\BoardSet;
             use Minesweeper\Game\BoardGenerator;
+            
+            $numofmines = 30;
 
-            $boardSet = BoardGenerator::generateBoard(5);
+            if(isset($_GET["numofmines"])) $numofmines = $_GET["numofmines"];
+
+            $boardSet = BoardGenerator::generateBoard($numofmines);
             $boardRenderer = new BoardRenderer($boardSet);
             $boardRenderer->show();
         ?>
