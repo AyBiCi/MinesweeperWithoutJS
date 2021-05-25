95d52d22f66324a121d045c171a52b2ce64d2a96
AyBiCi <rymcymcym100@gmail.com>
2021-05-25 21:43:36 +0200

    Implemented blank tiles discovery

--- Game/BoardSet.php
+++ Game/BoardSet.php
@@ -44,5 +44,18 @@ class BoardSet{
 
     public function reveal($x, $y){
         $this->tile_cover[$x][$y] = 0;
+        if($this->tiles[$x][$y] == 0)
+            $this->revealBlanks($x,$y);
+    }
+
+    public function revealBlanks($x, $y){
+        for($ex = ($x != 0 ? $x-1 : $x); $ex <= ($x != 19 ? $x+1 : $x);$ex++)
+        for($ey = ($y != 0 ? $y-1 : $y); $ey <= ($y != 19 ? $y+1 : $y);$ey++){
+            if($this->tile_cover[$ex][$ey] == 1){
+                $this->tile_cover[$ex][$ey] = 0;
+                if($this->tiles[$ex][$ey] == 0)
+                    $this->revealBlanks($ex, $ey);
+            }
+        }
     }
 }
